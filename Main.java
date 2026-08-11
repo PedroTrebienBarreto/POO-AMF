98d1954e2c35bf8c398b4f95d3f4d8c614022f10
Pedro Trebien Barreto <phtbarreto@gmail.com>
2026-08-11 20:24:17 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Para evitar erros pela entrada de dados pelo Scanner, é necessário remover do comentário os blocos individualmente caso queira rodar o código
+        // Atividade 2
+/**
         Filme BandOfBrothers = new Filme("Band of Brothers", 125, 9.8);
         Filme JamesBond007 = new Filme("007", 107, 9.3);
         Filme RealSteel = new Filme("Real Steel", 96, 9.7);
@@ -13,8 +15,9 @@ public class Main {
         JamesBond007.avaliar();
 
         JamesBond007.exibirFicha();
-
-
+ **/
+        // Atividade 1
+ /**
         Produto detergente = new Produto("Detergente", 3.50, 100);
         Produto bolaDeGude = new Produto("Bolinha de gude", 0.25, 1220);
         Produto enxada = new Produto("Enxada", 17.0, 8);
@@ -24,8 +27,9 @@ public class Main {
         enxada.exibirInfo();
 
         enxada.vender();
-
-
+ **/
+        // Atividade 3
+ /**
         ContaBancaria conta1 = new ContaBancaria("Joãozinho", 3200.0);
         ContaBancaria conta2 = new ContaBancaria("Maria",6530.0);
 
@@ -34,7 +38,7 @@ public class Main {
 
         conta1.exibirSaldo();
         conta2.exibirSaldo();
-
+**/
 
 
 
