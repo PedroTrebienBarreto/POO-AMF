10282879b7106c36f4856557952237e7c87e4592
Pedro Trebien Barreto <phtbarreto@gmail.com>
2026-08-17 21:12:35 -0300

    Create Cliente.java

---
+++ Aula03/Exercício01/Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+
+    private String name;
+    private String email;
+
+    public Cliente(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
