ccbfad5226591139c19bd9eeab343f0390181499
Pedro Trebien Barreto <phtbarreto@gmail.com>
2026-08-17 21:12:47 -0300

    Create Produto.java

---
+++ Aula03/Exercício01/Produto.java
@@ -0,0 +1,17 @@
+public class Produto {
+
+    private String name;
+    private Double price;
+
+    public Produto(String name, Double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public Double getPrice(){
+        return this.price;
+    }
+}
